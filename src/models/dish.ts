14c710a55451aa77a63973508132f85fa89dbc34
Poseidno228 <danil.t404@gmail.com>
2023-01-17 15:16:29 +0000

    Start of the first assigment

---
+++ src/models/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+  id!: string;
+  name!: string;
+  image: string | undefined;
+  category!: string;
+  featured!: boolean;
+  label: string | undefined;
+  price!: string;
+  description: string | undefined;
+}
